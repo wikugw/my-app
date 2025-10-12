c8a5b397180951a52452ff396761eccb59dd14c0
Wiku Galindra Wardhana <67788111+wikugw@users.noreply.github.com>
2025-10-12 13:35:38 +0700

    update vite config and node version

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,17 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
+import { fileURLToPath } from 'url';
 
-// https://vite.dev/config/
+// Needed because __dirname isn't defined in ESM
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 export default defineConfig({
   plugins: [react()],
-  css: {},
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'),
+      '@': path.resolve(__dirname, 'src'),
     },
   },
 });
